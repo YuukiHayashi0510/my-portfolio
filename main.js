e7016bc743acce6a5a43fb9fd7a41514f6a0ef2f
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2022-04-24 15:59:00 +0900

    second-container

--- main.js
+++ main.js
@@ -4,10 +4,15 @@ const imageElement = document.getElementById("image");
 const wakeupButton = document.getElementById("wakeup-button");
 const backButton = document.getElementById("back-button");
 const body = document.getElementById("body");
+const animeImage = document.getElementById("anime-image");
+const codeGeass = document.getElementById("code-geass");
+const geassButton = document.getElementById("geass");
+const rebornMessage = document.getElementById("reborn-message");
 
 // 真偽値
 let transName = false;
 let isWakeup = false;
+let isReborn = false;
 
 // setIntervalの秒数
 let sec = 3;
@@ -29,6 +34,14 @@ window.onload = function () {
 
 wakeupButton.onclick = function () {
   imageElement.src = "./images/wakeup.png";
+  imageElement.alt = "エヴァンゲリオン初号期擬似シン化";
+  wakeupButton.hidden = true;
+  backButton.hidden = false;
+  imageElement.scrollIntoView({ behavior: "smooth" });
+};
+
+wakeupButton.onclick = function () {
+  imageElement.src = "./images/wakeup.png";
   isWakeup = true;
   wakeupButton.hidden = true;
   backButton.hidden = false;
@@ -37,8 +50,24 @@ wakeupButton.onclick = function () {
 
 backButton.onclick = function () {
   imageElement.src = "./images/mia-nohara-RqKoA7A29x0-unsplash.jpeg";
+  if (isReborn === false) {
+    animeImage.src = "./images/eva01.png";
+  }
   isWakeup = false;
   wakeupButton.hidden = false;
   backButton.hidden = true;
   imageElement.scrollIntoView({ behavior: "smooth" });
+};
+
+codeGeass.onmouseover = function () {
+  if (isReborn === false) {
+    animeImage.src = "./images/lelouch.jpeg";
+  }
+};
+
+geassButton.onclick = function () {
+  animeImage.src = "./images/lelouch2.webp";
+  isReborn = true;
+  geassButton.hidden = true;
+  rebornMessage.hidden = false;
 };
